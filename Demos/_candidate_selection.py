ebd25754e57f20768c6161bb712933e3d3f75a12
Marc Paterno <paterno@fnal.gov>
2019-09-21 13:56:51 -0500

    Small code cleanups suggested by PyCharm

--- Demos/_candidate_selection.py
+++ Demos/_candidate_selection.py
@@ -1,41 +1,16 @@
-import os
-import sys
-from glob import glob
 from context import pandana
 from pandana.core import *
 from pandana.cut.analysis_cuts import kNumuCutND
 
-def main():
-  # import matplotlib.pyplot as plt
-  
-  kSlcE = Var(lambda tables: tables['rec.slc']['calE'])
-  
-  # Read data files.
-  #
-  # This needs to be improved, and will probably vastly change when we have one
-  # or few large (concatenated) files.
-  
-  input_directory = sys.argv[1]
-  maxfiles = 50
-  if (len(sys.argv) > 2):
-      maxfiles = int(sys.argv[2])
-  input_files = glob(os.path.join(input_directory, '*.h5caf.h5'))
-  tables = loader(input_files, limit=maxfiles)
-  
-  # Create a spectrum for the events passing our candidate cut.
-  myspectrum = spectrum(tables, kNumuCutND, kSlcE)
-  
-  # Let's do it!
-  tables.Go()
-  
-  print('myspectrum internal dataframe: ')
-  print((myspectrum.df().head()))
-  
-  n, bins = myspectrum.histogram(bins=50, range=(1, 4))
-  print('Selected ', n.sum(), ' events from ', myspectrum.POT(), '  POT.')
-  
-  # plt.hist(bins[:-1], bins=bins, weights=n, histtype='step', label='Candidates!')
-  # plt.xlabel('Slice calE')
-  # plt.ylabel('Events')
-  # plt.legend(loc='upper right')
-  # plt.show()
+
+def main(input_files, max_files):
+    tables = loader(input_files, limit=max_files)
+    energy = Var(lambda tables: tables['rec.slc']['calE'])
+    my_spectrum = spectrum(tables, kNumuCutND, energy)
+    tables.Go()
+
+    print('my_spectrum internal dataframe: ')
+    print((my_spectrum.df().head()))
+
+    n, _ = my_spectrum.histogram(bins=50, range=(1, 4))
+    print('Selected ', n.sum(), ' events from ', my_spectrum.POT(), '  POT.')
--- Demos/candidate_selection.py
+++ Demos/candidate_selection.py
@@ -1,3 +1,12 @@
-from _candidate_selection import main
+import _candidate_selection
+from glob import glob
+import os
+import sys
 
-main()
+input_directory = sys.argv[1]
+max_files = 50
+if len(sys.argv) > 2:
+    max_files = int(sys.argv[2])
+input_files = glob(os.path.join(input_directory, '*.h5caf.h5'))
+_candidate_selection.main(input_files, max_files)
+

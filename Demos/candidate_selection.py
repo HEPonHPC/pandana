acfba870f8b03016429961a0336dfb6ee3903348
Marc Paterno <paterno@fnal.gov>
2020-07-09 14:48:48 -0500

    Improve UI for candidate_selection program

--- Demos/candidate_selection.py
+++ Demos/candidate_selection.py
@@ -1,13 +1,16 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("input_directory", help = "directory from which to read input files")
+parser.add_argument("id_col", help = "name of the globally unique event id column")
+parser.add_argument("max_files", help = "maximum number of files to read", type = int)
+args = parser.parse_args()
+
 import _candidate_selection
 from glob import glob
 import os
 import sys
 
-input_directory = sys.argv[1]
-idcol = sys.argv[2]
-max_files = 50
-if len(sys.argv) > 3:
-    max_files = int(sys.argv[3])
-input_files = glob(os.path.join(input_directory, '*.h5caf.h5'))
-_candidate_selection.main(input_files, idcol, max_files)
+input_files = glob(os.path.join(args.input_directory, '*.h5caf.h5'))
+_candidate_selection.main(input_files, args.id_col, args.max_files)
 

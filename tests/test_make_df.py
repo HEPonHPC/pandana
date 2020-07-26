7ea050237a9b277caa32ad7914c8a317e81a0db5
Marc Paterno <paterno@fnal.gov>
2020-07-26 15:36:22 -0500

    Remove dependence on data file in fixed location

--- tests/test_make_df.py
+++ tests/test_make_df.py
@@ -2,23 +2,25 @@ from unittest import TestCase
 import h5py as h5
 import numpy as np
 import pandas as pd
-from pandana.utils.make_df import make_df
+from utils.make_df import make_df
+
 
 class TestMakeDF(TestCase):
     def setUp(self):
-        f = h5.File("/scratch-shared/paterno/small/small.h5caf.h5", "r")
+        # We need a small file to create and put into self.f
+        pass
 
     def tearDown(self):
-        f.close()
+        pass
 
     def test_read_list(self):
-        spill = pandana.utils.make_df(self.f, "spill", ["run", "subrun", "evt"])
+        spill = make_df(self.f, "spill", ["run", "subrun", "evt"])
         self.assertIsInstance(spill, pd.DataFrame)
         self.assertEqual(len(spill.index), 2453)
         self.assertEqual(spill.columns, ["run", "subrun", "evt"])
 
     def test_read_all(self):
-        spill = pandana.utils.make_df(self.f, "spill")
+        spill = make_df(self.f, "spill")
         self.assertIsInstance(spill, pd.DataFrame)
         self.assertEqual(len(spill.index), 2453)
         self.assertEqual(len(spill.columns), 54)

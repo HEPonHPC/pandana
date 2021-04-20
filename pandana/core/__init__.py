d3d2406fe3a15d4b9405832d1ac2b6f727555e8b
grohmc <grohmcfnal.gov>
2021-04-20 16:46:44 -0500

    Bring FilledSpectrum to the top.

--- pandana/core/__init__.py
+++ pandana/core/__init__.py
@@ -2,5 +2,5 @@ from pandana.core import *
 from pandana.core.loader import Loader
 from pandana.core.var import Var
 from pandana.core.cut import Cut
-from pandana.core.spectrum import Spectrum
+from pandana.core.spectrum import Spectrum,FilledSpectrum
 from pandana.core.datagroup import DataGroup
